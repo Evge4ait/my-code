c5c3ba9aaca06947e3269ce84639dfa212bb8037
Evge4ait <156699711+Evge4ait@users.noreply.github.com>
2025-03-12 19:01:41 +0600

    Update x4a.dart

--- x4a.dart
+++ x4a.dart
@@ -1,28 +1,29 @@
 void main() {
   makeTask(() => doSomething(10)); // передает заданную температуру
+  makeTask(() => doSomething(-300)); // для теста на ошибку
 }
 
 String checkFreezing(int temp) =>
-    temp < 0 ? 'very cold' : 'not freezing'; // проверка на мороз надо не позабыть про not freezing !!
+    temp < 0 ? 'very cold' : 'not freezing'; // проверка на мороз
 
 String checkTemperature(int temp) {
-  const freezing = 0; // меняемые константы
+  const freezing = 0; // настраиваемые значения
   const coolMax = 15;
   const warmMax = 25;
 
+  if (temp < -273) {
+    throw Exception('Incorrect temperature $temp°C ');
+  }
   bool isFreezing(int temp) => temp < 0; // проверка что температура ниже нуля
   if (isFreezing(temp)) {
-    return 'very cold';
-  }
-
-  if (temp >= freezing && temp <= coolMax) {
-    // проверка на 0 <= t <= 15
-    return 'cold'; // если верно возввращает 'cold' если нет идем дальше
-  } else if (temp > coolMax && temp <= warmMax) {
-    // проверка на 16 > t <= 25
-    return 'warm'; // если верно возвращает 'warm' если нет идем дальше
+    return 'very cold $temp°C';
+  } else if (temp <= coolMax) {
+    return 'cold $temp°C';
+  } else if (temp <= warmMax) {
+    // предыдущие условия исключают t <= coolMax
+    return 'warm $temp°C';
   } else {
-    return 'very warm'; // если t выше и не подходит по параметрам то t = very varm
+    return 'very warm $temp°C';
   }
 }
 
@@ -32,5 +33,11 @@ void makeTask(Function task) {
   print('Ready'); // завершение выполнения функции
 }
 
-void doSomething(int temp) =>
-    print('Weather outside: ${checkTemperature(temp)}');
+void doSomething(int temp) {
+  try {
+    String result = checkTemperature(temp);
+    print('Weathr outside: $result');
+  } catch (e) {
+    print('Error: ${e.toString()}');
+  }
+}
